e480c31e8a49caf14654ea3e5ca6bde1669e088d
Robot.l <lbh9311@sina.com>
2023-12-01 15:26:58 +0800

    fix: tg script

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -18,19 +18,7 @@ const Login: FC = () => {
           <p className="text-center">{`Hi, ${initDataUnsafe?.user?.first_name}`}</p>
         </div>
       )}
-      <script
-        async
-        src="https://telegram.org/js/telegram-widget.js?22"
-        data-telegram-login="alpha_bot"
-        data-size="large"
-        data-onauth="onTelegramAuth(user)"
-        data-request-access="write"
-      ></script>
-      <script type="text/javascript">
-        {`function onTelegramAuth(user) {
-          alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-        }`}
-      </script>
+      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="alpha_bot" data-size="large" data-auth-url="https://brc20-inscribe-bot.vercel.app/"></script>
     </div>
   );
 };
