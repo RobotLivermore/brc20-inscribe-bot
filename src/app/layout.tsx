752cf5fd42b8d3de452517014518fb122c81ef00
Robot.l <lbh9311@sina.com>
2023-12-01 15:17:44 +0800

    feat: add tg login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from "next/script";
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="beforeInteractive"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -3,16 +3,34 @@
 import { FC, useEffect, useState } from "react";
 
 const Login: FC = () => {
-  const [hash, setHash] = useState('')
-
+  const [initDataUnsafe, setInitDataUnsafe] = useState<any | null>(null);
   useEffect(() => {
-    // console.log("Hash:", window.location.hash);
-    setHash(window.location.hash)
+    if ((window as any)?.Telegram?.WebApp?.initDataUnsafe) {
+      console.log((window as any).Telegram?.WebApp?.initDataUnsafe);
+      setInitDataUnsafe((window as any)?.Telegram?.WebApp?.initDataUnsafe);
+    }
   }, []);
+
   return (
     <div>
-      <h1>login</h1>
-      <span className="break-all">{hash}</span>
+      {initDataUnsafe?.user && (
+        <div className="flex justify-between py-2">
+          <p className="text-center">{`Hi, ${initDataUnsafe?.user?.first_name}`}</p>
+        </div>
+      )}
+      <script
+        async
+        src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login="alpha_bot"
+        data-size="large"
+        data-onauth="onTelegramAuth(user)"
+        data-request-access="write"
+      ></script>
+      <script type="text/javascript">
+        {`function onTelegramAuth(user) {
+          alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+        }`}
+      </script>
     </div>
   );
 };
